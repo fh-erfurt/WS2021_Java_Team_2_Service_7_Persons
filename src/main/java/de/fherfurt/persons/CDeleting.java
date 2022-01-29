cdae150222372def406cf040e7482e5ae2adfe1a
Yatamashi <81873469+Yatamashi@users.noreply.github.com>
2022-01-29 19:39:12 +0100

    Version 1.1-Add class Deleting

---
+++ src/main/java/de/fherfurt/persons/CDeleting.java
@@ -0,0 +1,24 @@
+package de.fherfurt.persons;
+
+public class CDeleting
+{
+    private Boolean inDelete;
+    private int HashMapKey;
+
+    public CDeleting(Boolean inDelete, int HashMapKey) // Konstruktor
+    {
+        this.inDelete = inDelete;
+        this.HashMapKey = HashMapKey;
+    }
+
+    void SearchHashMapKey()
+    {
+
+    }
+
+    void deleteHashMapKey()
+    {
+
+    }
+
+}
